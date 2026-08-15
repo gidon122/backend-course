5a54d4d3ec98f1579ff6cb813aacbb8f09f3f181
Ufaruna Gideon <ufarunagidosky@gmail.com>
2026-08-15 04:47:21 +0100

    backend

--- server.js
+++ server.js
@@ -1,14 +1,29 @@
-const http = require("http");
+// const http = require("http");
 
-const HOSTNAME = process.env.HOSTNAME || "localhost";
-const PORT = process.env.PORT || 3000;
+// const HOSTNAME = process.env.HOSTNAME || "localhost";
+// const PORT = process.env.PORT || 3000;
 
-const server = http.createServer((request, response) => {
-    response.statusCode = 200;
-    response.setHeader("Content-Type", "text/plain");
-    response.end("Hello World\n");  
-});
+// const server = http.createServer((request, response) => {
+//     response.statusCode = 200;
+//     response.setHeader("Content-Type", "text/plain");
+//     response.end("Hello World\n");  
+// });
 
-server.listen(PORT, HOSTNAME, () => {
-    console.log(`Server running at http://${HOSTNAME}:${PORT}/`);
-});
+// server.listen(PORT, HOSTNAME, () => {
+//     console.log(`Server running at http://${HOSTNAME}:${PORT}/`);
+// });
+
+
+// console.log(__filename)
+// console.log(__dirname)
+
+const fs = require("fs");
+
+try{
+    const data = fs.readFileSync("hi", "utf8");
+    console.log(data);
+} catch (err) {
+    console.error(err)
+}
+
+console.log("Log from outside");
